4ae6b4c19eb394635d6ef81a4dce6c4cd19d9e21
decafmind <decafmind@gmail.com>
2015-10-11 16:26:39 +0800

    XXX
    xxx

--- X-O-Genesis/Forms/SplashScreen.Designer.cs
+++ X-O-Genesis/Forms/SplashScreen.Designer.cs
@@ -60,8 +60,10 @@
             this.ClientSize = new System.Drawing.Size(616, 349);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "SplashScreen";
             this.Opacity = 0.9D;
+            this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "SplashScreen";
             this.TopMost = true;
