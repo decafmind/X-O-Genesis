132eb359eb1bfa07092ed6b832a511474866d252
decafmind <decafmind@gmail.com>
2016-01-26 12:36:36 +0800

    "Today's Sales" not "Today Sale"

--- X-O-Genesis/Control/Sales.Designer.cs
+++ X-O-Genesis/Control/Sales.Designer.cs
@@ -516,7 +516,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(274, 38);
             this.label1.TabIndex = 0;
-            this.label1.Text = "TODAY SALES";
+            this.label1.Text = "TODAY\'S SALES";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
